29e6b69741129afffcd7fd0bc1fadd9179c1b339
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-06-28 22:33:00 -0400

    center text

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ text_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/Ground.png').convert()
-text_surface = text_font.render('bol', False, 'black')
+
+score_surface = text_font.render('bol', False, 'black')
+score_rect = score_surface.get_rect(center = (400, 50))
 
 snail_surface = pygame.image.load('graphics/snail/Snail1.png').convert_alpha()
 snail_rect = snail_surface.get_rect(midbottom = (600, 300))
@@ -25,7 +27,7 @@ while True:
             
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (10, 10))
+    screen.blit(score_surface, score_rect)
 
     snail_rect.x -= 4 
     if snail_rect.right <= 0: snail_rect.left = 800
